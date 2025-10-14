3cc62b117b8be2d946ec4c9305f2724d9d1ba89e
Clement Dedessuslesmoustier <cde@niboo.com>
2025-10-14 16:30:57 +0200

    [MIG] delivery_roulier_option: Migration to 18.0

---
+++ delivery_roulier_option/__manifest__.py
@@ -0,0 +1,21 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Delivery Roulier Option",
+    "version": "18.0.1.0.0",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "summary": "Add options to roulier modules",
+    "maintainers": ["florian-dacosta"],
+    "category": "Inventory/Delivery",
+    "depends": [
+        "delivery_carrier_option",
+        "delivery_roulier",
+        "product_harmonized_system",  # from OCA/intrastat-extrastat
+    ],
+    "website": "https://github.com/OCA/delivery-carrier",
+    "data": [
+        "data/delivery.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
